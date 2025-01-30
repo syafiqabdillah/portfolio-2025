62b3489577bcc8b56798ff8b552707c7834acd1a
Syafiq Abdillah <abdillah.syafiq@gmail.com>
2025-01-30 10:43:55 +0700

    chore: udpate image location

--- src/constants.ts
+++ src/constants.ts
@@ -56,14 +56,14 @@ export const projects: Project[] = [
   {
     name: "Jlantah Hero",
     desc: "A mobile-first web app that lets you convert your used vegetable oil into money or new oils. It was our submission in Indonesia Digital Tribe 2021-2022 event that led us to Top 7 position out of 2000+ teams. IG: jlantah.hero",
-    src: "/src/assets/img/jlantahhero.png",
+    src: "/public/img/jlantahhero.png",
     url: "https://jlantahhero.netlify.app/",
     tech: ["Next.js", "React.js", "MongoDB", "Express.js", "Mixpanel"],
   },
   {
     name: "Not My Anime List",
     desc: "My spin-off of MyAnimeList using their AniList API. You can search anime and manage your anime collections.",
-    src: "/src/assets/img/notmal.png",
+    src: "/public/img/notmal.png",
     url: "https://notmal.netlify.app",
     tech: [
       "Typescript",
@@ -78,7 +78,7 @@ export const projects: Project[] = [
   {
     name: "Makanapaya",
     desc: "A website that helps you choose what to eat with hundreds of daily active users on its peak (now inactive). You can roll, look at nearby locations and recipes, give comments, and ban certain foods based on your preferences. This one is the most exciting personal project I've ever done.",
-    src: "/src/assets/img/makanapaya.png",
+    src: "/public/img/makanapaya.png",
     url: "https://www.makanapaya.my.id",
     tech: [
       "Next.js",
@@ -93,7 +93,7 @@ export const projects: Project[] = [
   {
     name: "Kenal Kampus",
     desc: "Collection of stories from students and alumni in universities all over Indonesia. It helps high school student to choose where to pursue their study. Entries are submitted by the students themselves and curated by me.",
-    src: "/src/assets/img/kenalkampus.png",
+    src: "/public/img/kenalkampus.png",
     url: "https://kenalkampus.vercel.app/",
     tech: [
       "Nuxt.js",
@@ -108,21 +108,21 @@ export const projects: Project[] = [
   {
     name: "Foodle",
     desc: "My spin-off of the viral word game in 2021-2022, Wordle, but the words are from Makanapaya's database.",
-    src: "/src/assets/img/foodle.png",
+    src: "/public/img/foodle.png",
     url: "https://www.makanapaya.my.id/foodle",
     tech: ["Next.js", "React.js", "MongoDB", "Express.js"],
   },
   {
     name: "Makanapaya Android",
     desc: "Android version of Makanapaya (inactive).",
-    src: "/src/assets/img/makanapayaandroid.jpg",
+    src: "/public/img/makanapayaandroid.jpg",
     url: "https://play.google.com/store/apps/details?id=com.syafiqau.makanapaya",
     tech: ["Flutter", "Dart", "MongoDB", "Express.js", "Mixpanel"],
   },
   {
     name: "Expensez",
     desc: "A simple expense tracker application. One active user (myself).",
-    src: "/src/assets/img/expensez.jpg",
+    src: "/public/img/expensez.jpg",
     url: "https://play.google.com/store/apps/details?id=com.syafiqabdillah.expensez",
     tech: ["Flutter", "Dart"],
   },

38ae26a0a24193ca36a2e5f17119523b82259827
Syafiq Abdillah <abdillah.syafiq@gmail.com>
2025-02-11 13:40:54 +0700

    remove open google search on clicking skill item

--- src/component/Skill.svelte
+++ src/component/Skill.svelte
@@ -11,27 +11,22 @@
       }
     }
   }
-
-  const openLink = (query: string) => {
-    window.open(`https://www.google.com/search?q=${query}`, "_blank");
-  };
 </script>
 
 <Container id="skill">
   <h2>Skills</h2>
   <div class="flex flex-wrap gap-4 px-4 justify-center max-w-2xl">
     {#each skills as skill}
-      <button
+      <div
         class={twMerge(
-          "py-2 px-3 rounded-md hover:scale-[1.5] transition-all cursor-pointer",
+          "py-2 px-3 rounded-md",
           theme.isDark
-            ? "text-zinc-700 bg-zinc-200 hover:text-zinc-800 hover:font-semibold"
+            ? "text-zinc-700 bg-zinc-200 hover:text-zinc-800"
             : "text-zinc-300 bg-zinc-500"
         )}
-        onclick={() => openLink(skill)}
       >
         {skill}
-      </button>
+      </div>
     {/each}
   </div>
 </Container>
